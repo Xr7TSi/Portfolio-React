20140984b859c51bb2e5c69c8eba90a81bcb4fe0
Jonathan Rein <jrein1296@gmail.com>
2021-07-07 22:16:53 -0400

    Adding conditional rendering to navbar in Nav.js.

--- src/components/Container.js
+++ src/components/Container.js
@@ -41,33 +41,5 @@ export default function Container() {
 
 
 
-// import React, { useState } from 'react';
-// import Nav from './Nav';
-// import About from './pages/About';
-// import Work from './pages/Work';
-// import WorkMobile from './pages/WorkMobile';
-// import Contact from './pages/Contact';
 
-// export default function Container() {
-//   const [currentPage, setCurrentPage] = useState('About');
-
-//   const renderPage = () => {
-//     if (currentPage === 'About') {
-//       return <About />;
-//     }
-//     if (currentPage === 'Contact') {
-//       return <Contact />;
-//     }
-//     return <Work />;
-//   };
-
-//   const handlePageChange = (page) => setCurrentPage(page);
-
-//   return (
-//     <div>
-//       <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
-//       {renderPage()}
-//     </div>
-//   );
-// }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,9 +23,84 @@ const styles = {
 
 
 
-function Nav({ currentPage, handlePageChange }) {
-    return (
+  // function Nav({ currentPage, handlePageChange }) {
+  //   return (
       
+  //     <ul className="nav nav-tabs"style={styles.navbarBackground}>
+  //         <a className="navbar-brand" style={styles.navbarLogo} >Jonathan Rein</a>
+  //       <li className="nav-item">
+  //         <a style={styles.navbarLocation}
+  //           href="#About"
+  //           onClick={() => handlePageChange('About')}
+  //           className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+  //         >
+  //           About
+  //         </a>
+  //       </li>
+  //       <li className="nav-item">
+  //         <a style={styles.navbarLocation}
+  //           href="#work"
+  //           onClick={() => handlePageChange('Work')}
+  //           className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}
+  //         >
+  //           Work
+  //         </a>
+  //       </li>
+    
+  //       <li className="nav-item">
+  //         <a style={styles.navbarLocation}
+  //           href="#contact"
+  //           onClick={() => handlePageChange('Contact')}
+  //           className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+  //         >
+  //           Contact
+  //         </a>
+  //       </li>
+  //     </ul>
+  //   );
+  // }
+
+
+  function Nav({ currentPage, handlePageChange }) { 
+    if (window.innerWidth <= 760)
+    return (
+
+      <ul className="nav nav-tabs"style={styles.navbarBackground}>
+          <a className="navbar-brand" style={styles.navbarLogo} >Jonathan Rein &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+        <li className="nav-item">
+          <a style={styles.navbarLocation}
+            href="#About"
+            onClick={() => handlePageChange('About')}
+            className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          >
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a style={styles.navbarLocation}
+            href="#work"
+            onClick={() => handlePageChange('Work')}
+            className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}
+          >
+            Work
+          </a>
+        </li>
+    
+        <li className="nav-item">
+          <a style={styles.navbarLocation}
+            href="#contact"
+            onClick={() => handlePageChange('Contact')}
+            className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+    ); 
+
+    if (window.innerWidth >= 761)
+    return (
+
       <ul className="nav nav-tabs"style={styles.navbarBackground}>
           <a className="navbar-brand" style={styles.navbarLogo} >Jonathan Rein</a>
         <li className="nav-item">
@@ -57,12 +132,8 @@ function Nav({ currentPage, handlePageChange }) {
           </a>
         </li>
       </ul>
-    );
+    ); 
   }
-
-
-
-
 
 
 export default Nav;
