c3879ec9a7f307075aeb7febcc814b673ab861cf
Jonathan Rein <jrein1296@gmail.com>
2021-07-07 21:21:57 -0400

    Added conditional rendering for Work.js or WorkMobile.js based on user screen size in Container.js.

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './Nav';
 import About from './pages/About';
 import Work from './pages/Work';
+import WorkMobile from './pages/WorkMobile';
 import Contact from './pages/Contact';
 
 export default function Container() {
@@ -14,7 +15,13 @@ export default function Container() {
     if (currentPage === 'Contact') {
       return <Contact />;
     }
-    return <Work />;
+    if (currentPage === 'Work' && window.innerWidth <= 760) {
+      return <WorkMobile />;
+    }
+    if (currentPage === 'Work') {
+      return <Work />;
+    }
+    
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
@@ -26,3 +33,41 @@ export default function Container() {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import Nav from './Nav';
+// import About from './pages/About';
+// import Work from './pages/Work';
+// import WorkMobile from './pages/WorkMobile';
+// import Contact from './pages/Contact';
+
+// export default function Container() {
+//   const [currentPage, setCurrentPage] = useState('About');
+
+//   const renderPage = () => {
+//     if (currentPage === 'About') {
+//       return <About />;
+//     }
+//     if (currentPage === 'Contact') {
+//       return <Contact />;
+//     }
+//     return <Work />;
+//   };
+
+//   const handlePageChange = (page) => setCurrentPage(page);
+
+//   return (
+//     <div>
+//       <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+//       {renderPage()}
+//     </div>
+//   );
+// }
+
