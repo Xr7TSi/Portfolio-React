ad900e72cde36752e6c90715f9558deb972ea7ac
Jonathan Rein <jrein1296@gmail.com>
2021-08-18 17:32:39 -0400

    Adding typist feature to home page.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typist from 'react-typist';
 
 
 
@@ -7,7 +8,16 @@ function About() {
   <div className="container" id="home-text">
       <div className="col-md-12">
           <h1>Hi. I'm Jonathan.  </h1>
+          <br></br>
+          <Typist>
           <h3>I'm a full-stack web developer.</h3>
+          <Typist.Delay ms={500} />
+          <h3> I work with React.js, Express, Node.js, and MongoDB.</h3>
+          <Typist.Delay ms={1000} />
+          <h3> Click "Work" to see some of my projects.</h3>
+          </Typist>
+      
+          
       </div>
   </div>
   );
